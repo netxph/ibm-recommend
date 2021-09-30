bb7f21e4591e2b469a380ab620a9a1af4756c602
Marc Vitalis <marc.vitalis@live.com>
2021-09-30 18:53:09 +0800

    baseline

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ibm_recommend',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Recommendations with IBM',
+    author='Marc Vitalis',
+    license='MIT',
+)
